37f15676cc2a9917949a8fec90036bc14958dc15
Nerentra <60801196+Nerentra@users.noreply.github.com>
2024-10-30 18:18:39 -0700

    Add documentation to skeleton.js

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -1,3 +1,9 @@
+/**
+ * Inserts an html template into the page.
+ *
+ * @param {string} className The classname to insert into.
+ * @param {string} templateName The filename of the html in /templates.
+ */
 function addTemplate(className, templateName) {
   let elem = document.querySelector("." + className);
   let url = `/templates/${templateName}.html`;
@@ -15,17 +21,13 @@ function addTemplate(className, templateName) {
   }
 }
 
-function loadSkeleton() {
-  firebase.auth().onAuthStateChanged(function (user) {
-    if (user) {
-      // User is signed in.
-      addTemplate("navbarTemplate", "navbarPostlogin");
-    } else {
-      // No user is signed in.
-      addTemplate("navbarTemplate", "navbarPrelogin");
-    }
-    addTemplate("footerTemplate", "footer");
-  });
-}
-
-loadSkeleton();
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    // User is signed in.
+    addTemplate("navbarTemplate", "navbarPostlogin");
+  } else {
+    // No user is signed in.
+    addTemplate("navbarTemplate", "navbarPrelogin");
+  }
+});
+addTemplate("footerTemplate", "footer");
