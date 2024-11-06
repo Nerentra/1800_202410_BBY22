2c37905fd02fba6dcf0eeaff9500b6b4d11aa2ba
Nerentra <60801196+Nerentra@users.noreply.github.com>
2024-11-06 15:14:20 -0800

    Refactor question.js a little

--- scripts/question.js
+++ scripts/question.js
@@ -1,67 +1,68 @@
-
 /**
- * @params
- * 
- * get data from firebase and display it on the page
+ * Get data from firebase and display it on the page
  */
 function displayQuestion() {
-    let params = new URL( window.location.href ); //get URL of search bar
-    let ID = params.searchParams.get( "docID" ); //get value for key "id"
-    console.log( ID );
+  let params = new URL(window.location.href); // Get URL of search bar
+  let ID = params.searchParams.get("docID"); // Get value for key "id"
 
-    // getting info from the firebase collection
-    db.collection( "questions" )
-        .doc( ID )
+  // Getting info from the firebase collection
+  db.collection("questions")
+    .doc(ID)
+    .get()
+    .then((doc) => {
+      let docData = doc.data();
+      let question = docData;
+      let title = docData.title;
+      let description = docData.description;
+      let author = docData.author;
+      let timestamp = docData.timestamp;
+
+      let timeElapsed = Date.now() - timestamp;
+      let readableTime = makeDurationReadable(timeElapsed);
+
+      // Only populate title, and image
+      document.getElementById("questionTitle").innerText = title;
+      document.getElementById("question-description").innerText = description;
+      document.getElementById("questionAuthor").innerText = author;
+      document.getElementById("questionTimestamp").innerText = readableTime;
+      // let imgEvent = document.querySelector( ".hike-img" );
+      // imgEvent.src = "../images/" + hikeCode + ".jpg";
+
+      question.author
         .get()
-        .then( doc => {
-            question = doc.data();
-            title = doc.data().title;
-            description = doc.data().description;
-            author = doc.data().author;
-            timestamp = doc.data().timestamp;
-
-            question.author.get().then(authorDoc => {
-                let authorName = authorDoc.data().name || "Unknown Author";
-                document.getElementById("questionAuthor").innerHTML = authorName;
-            }).catch(error => {
-                console.error("Error fetching author:", error);
-                document.getElementById("questionAuthor").innerHTML = "Author not found";
-            });
-
-            timeElapsed = Date.now() - timestamp;
-            readableTime = makeDurationReadable(timeElapsed);
-            
-            // only populate title, and image
-            document.getElementById( "questionTitle" ).innerHTML = title;
-            document.getElementById("question-description").innerHTML = description;
-            document.getElementById("questionAuthor").innerHTML = author;
-            document.getElementById("questionTimestamp").innerHTML = readableTime;
-            // let imgEvent = document.querySelector( ".hike-img" );
-            // imgEvent.src = "../images/" + hikeCode + ".jpg";
-        } );
+        .then((authorDoc) => {
+          let authorName = authorDoc.data().name || "Unknown Author";
+          document.getElementById("questionAuthor").innerText = authorName;
+        })
+        .catch((error) => {
+          console.error("Error fetching author:", error);
+          document.getElementById("questionAuthor").innerText =
+            "Author not found";
+        });
+    });
 }
 displayQuestion();
 
-// copied from search.js
+// Copied from search.js
 /**
- * @author giorgio
  * Takes a duration in milliseconds and returns
  * a time with appropriate units.
+ * @author Giorgio
  * @param {Number} The duration in milliseconds.
  * @returns {String} The formatted time with units.
  */
 function makeDurationReadable(duration) {
-    const units = ["s", "m", "h", "d", "y"];
-    const conversions = [1000, 60, 60, 24, 365];
-    for (let i = 0; i < conversions.length - 1; i++) {
-      duration /= conversions[i];
-      if (duration < conversions[i + 1]) {
-        return Math.floor(duration) + units[i];
-      }
+  const units = ["s", "m", "h", "d", "y"];
+  const conversions = [1000, 60, 60, 24, 365];
+  for (let i = 0; i < conversions.length - 1; i++) {
+    duration /= conversions[i];
+    if (duration < conversions[i + 1]) {
+      return Math.floor(duration) + units[i];
     }
-    return Math.floor(duration) + units[units.length - 1];
   }
-  
+  return Math.floor(duration) + units[units.length - 1];
+}
+
 /**
  * some function i created to get the firebase data and insert it into the webpage
  * displayQuestion works just fine though
@@ -86,3 +87,4 @@ function makeDurationReadable(duration) {
 //             document.getElementById("question-description").innerHTML = questionDesc;
 //         });
 // }
+
